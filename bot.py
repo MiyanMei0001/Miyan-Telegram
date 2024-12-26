9fbc19f96d2ffd28538d22115f08e53ca21ddc50
MiyanMei0001 <miyanmei0001@gmail.com>
2024-12-27 00:41:08 +0700

    Update

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 import telebot
 from pymongo import MongoClient
 from flask import Flask, request
-from bson.objectid import ObjectId
-import requests
 
 load_dotenv()
 
@@ -19,124 +17,70 @@ files_collection = db['files']
 
 app = Flask(__name__)
 
-MAX_FILE_SIZE_MB = 20 # Set your maximum size here
-
 def get_file_info(file_id):
     file_info = bot.get_file(file_id)
     return file_info
-
-def file_info_to_dict(file_info):
-    if file_info:
-        return {
-            "file_id": file_info.file_id,
-            "file_unique_id": file_info.file_unique_id,
-            "file_size": file_info.file_size,
-            "file_path": file_info.file_path,
-        }
-    return None
-
-def get_file_url(file_id):
-    file_info = get_file_info(file_id)
-    file_path = file_info.file_path
-    return f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_path}"
-
-def is_valid_file_id(file_id):
-    try:
-        file_info = bot.get_file(file_id)
-        if file_info:
-            return True
-    except telebot.apihelper.ApiException:
-        return False
 
 def get_media_type(message):
     if message.reply_to_message:
         message = message.reply_to_message
         
     if message.document:
-        return 'document', message.document.file_id, message.document.file_size
+        return 'document', message.document.file_id
     elif message.photo:
-        return 'photo', message.photo[-1].file_id, message.photo[-1].file_size
+        return 'photo', message.photo[-1].file_id
     elif message.video:
-        return 'video', message.video.file_id, message.video.file_size
+        return 'video', message.video.file_id
     elif message.audio:
-        return 'audio', message.audio.file_id, message.audio.file_size
+        return 'audio', message.audio.file_id
     elif message.voice:
-        return 'voice', message.voice.file_id, message.voice.file_size
+        return 'voice', message.voice.file_id
     elif message.sticker:
-        return 'sticker', message.sticker.file_id, None
+        return 'sticker', message.sticker.file_id
     else:
-        return None, None, None
+        return None, None
 
-def download_file(file_path, file_name):
-    try:
-        file_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_path}"
-        response = requests.get(file_url, stream=True)
-        response.raise_for_status()
-        
-        with open(file_name, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
-        return True
-    except Exception as e:
-        print(f"Error downloading file: {e}")
-        return False
+def forward_message_and_get_message_id(chat_id, from_chat_id, message_id):
+    forwarded_message = bot.forward_message(chat_id, from_chat_id, message_id)
+    return forwarded_message.message_id
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello! I am a bot to save and send media.\n\nUsage:\n/save [name] - to save media with a name\n/send [name] - to send media based on the name")
+    bot.reply_to(message, "Hello! I am a bot for saving and sending media.\n\nUse:\n/save [name] - to save media with a name\n/send [name] - to send media based on the name")
 
 @bot.message_handler(commands=['save'])
 def save_media(message):
     try:
         command_parts = message.text.split(' ', 1)
         if len(command_parts) < 2:
-            bot.reply_to(message, "Invalid format, use: /save [name]")
+            bot.reply_to(message, "Incorrect format, use: /save [name]")
             return
 
         name = command_parts[1].strip()
-        media_type, file_id, file_size = get_media_type(message)
-        
+        media_type, file_id = get_media_type(message)
+
         if not file_id:
-            bot.reply_to(message, "Sorry, this command can only save media files (documents, photos, videos, audio, etc).")
-            return
-        
+             bot.reply_to(message, "Sorry, this command can only save media files (documents, photos, videos, audio, etc.)")
+             return
+
         existing_file = files_collection.find_one({"name": name})
-        
+
         if existing_file:
-            bot.reply_to(message, "Sorry, a file with this name already exists. Please use a different name.")
-            return
-        
-        file_info = get_file_info(file_id)
-        
-        is_large_file = False
-        if file_size and file_size > MAX_FILE_SIZE_MB * 1024 * 1024:
-            is_large_file = True
-        
-        file_data = {
+            bot.reply_to(message, "Sorry, the file name already exists. Please use another name.")
+        else:
+            forward_message_id = forward_message_and_get_message_id(message.chat.id, message.chat.id, message.message_id)
+            file_info = get_file_info(file_id)
+            file_data = {
                 "name": name,
-                "file_id": file_id if not is_large_file else None,  # Set file_id to None for large files
+                "file_id": file_id,
                 "media_type": media_type,
                 "user_id": message.from_user.id,
                 "username": message.from_user.username,
-                "file_info": file_info_to_dict(file_info),
-                "save_type": "local" if is_large_file else "online" # set to local if is large file
+                "message_id": forward_message_id,
+                "file_info": file_info
             }
-
-        if is_large_file:
-             file_path = file_info.file_path
-             if download_file(file_path,name):
-                  file_data["file_path"] = name
-             else:
-                 bot.reply_to(message, f"Failed to download the file {name}")
-                 return
-        
-        elif not is_valid_file_id(file_id):
-            bot.reply_to(message, "File Id is invalid")
-            return
-             
-        files_collection.insert_one(file_data)
-        bot.reply_to(message, f"Media with name '{name}' successfully saved.")
-
+            files_collection.insert_one(file_data)
+            bot.reply_to(message, f"Media with the name '{name}' has been successfully saved.")
     except Exception as e:
         bot.reply_to(message, f"An error occurred: {str(e)}")
 
@@ -145,50 +89,15 @@ def send_media(message):
     try:
         command_parts = message.text.split(' ', 1)
         if len(command_parts) < 2:
-            bot.reply_to(message, "Invalid format, use: /send [name]")
+            bot.reply_to(message, "Incorrect format, use: /send [name]")
             return
 
         name = command_parts[1].strip()
         file_data = files_collection.find_one({"name": name})
 
         if file_data:
-            file_id = file_data.get("file_id")
-            media_type = file_data["media_type"]
-            save_type = file_data.get("save_type", "online") # always should be "local" or "online"
-            
-            if save_type == "online" and file_id:
-                if media_type == 'document':
-                    bot.send_document(message.chat.id, file_id, caption=f"File: {name} ({file_data.get('username')})")
-                elif media_type == 'photo':
-                    bot.send_photo(message.chat.id, file_id, caption=f"Photo: {name} ({file_data.get('username')})")
-                elif media_type == 'video':
-                    bot.send_video(message.chat.id, file_id, caption=f"Video: {name} ({file_data.get('username')})")
-                elif media_type == 'audio':
-                    bot.send_audio(message.chat.id, file_id, caption=f"Audio: {name} ({file_data.get('username')})")
-                elif media_type == 'voice':
-                    bot.send_voice(message.chat.id, file_id, caption=f"Voice Note: {name} ({file_data.get('username')})")
-                elif media_type == 'sticker':
-                    bot.send_sticker(message.chat.id, file_id)
-                else:
-                    bot.reply_to(message, "Unknown media type.")
-            elif save_type == "local":
-                file_path = file_data.get("file_path")
-                if file_path:
-                    if media_type == 'document':
-                        bot.send_document(message.chat.id, open(file_path,'rb'), caption=f"File: {name} ({file_data.get('username')})")
-                    elif media_type == 'photo':
-                        bot.send_photo(message.chat.id, open(file_path,'rb'), caption=f"Photo: {name} ({file_data.get('username')})")
-                    elif media_type == 'video':
-                       bot.send_video(message.chat.id, open(file_path,'rb'), caption=f"Video: {name} ({file_data.get('username')})")
-                    elif media_type == 'audio':
-                        bot.send_audio(message.chat.id, open(file_path,'rb'), caption=f"Audio: {name} ({file_data.get('username')})")
-                    elif media_type == 'voice':
-                        bot.send_voice(message.chat.id, open(file_path,'rb'), caption=f"Voice Note: {name} ({file_data.get('username')})")
-                    elif media_type == 'sticker':
-                           bot.send_sticker(message.chat.id, open(file_path,'rb'))
-                else:
-                     bot.reply_to(message, f"File not found in local {name}")
-            
+            message_id = file_data["message_id"]
+            bot.forward_message(message.chat.id, message.chat.id, message_id)
         else:
             bot.reply_to(message, f"No media found with the name '{name}'")
     except Exception as e:
